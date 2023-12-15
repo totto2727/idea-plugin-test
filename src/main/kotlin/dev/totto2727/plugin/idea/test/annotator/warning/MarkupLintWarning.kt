e8a777ab99e5c45ccc938031e420020351ae2cec
totto2727 <h_tsuchida@yumemi.co.jp>
2023-12-15 11:07:34 +0900

    wip

--- src/main/kotlin/dev/totto2727/plugin/idea/test/annotator/warning/MarkupLintWarning.kt
+++ src/main/kotlin/dev/totto2727/plugin/idea/test/annotator/warning/MarkupLintWarning.kt
@@ -1,6 +1,12 @@
 package dev.totto2727.plugin.idea.test.annotator.warning
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonNamingStrategy
+import kotlinx.serialization.json.decodeFromStream
+import java.io.InputStream
+
 
 @Serializable
 data class MarkupLintWarning(
@@ -8,4 +14,12 @@ data class MarkupLintWarning(
     val column: Int,
     val reason: String
 ) {
+    @OptIn(ExperimentalSerializationApi::class)
+    companion object {
+        private val decoder = Json {
+            namingStrategy = JsonNamingStrategy.SnakeCase
+        }
+
+        fun fromJson(stream: InputStream) = decoder.decodeFromStream<List<MarkupLintWarning>>(stream)
+    }
 }
